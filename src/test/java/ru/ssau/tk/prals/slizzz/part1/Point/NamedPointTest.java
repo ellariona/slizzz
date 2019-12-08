0b68298bdda14ed48191ddc6803f5208a9f09bb5
Елизавета <luhra@mail.ru>
2019-12-09 00:20:36 +0400

    task 1.16

---
+++ src/test/java/ru/ssau/tk/prals/slizzz/part1/Point/NamedPointTest.java
@@ -0,0 +1,29 @@
+package ru.ssau.tk.prals.slizzz.part1.Point;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class NamedPointTest {
+
+    NamedPoint firstPoint = new NamedPoint();
+    NamedPoint secondPoint = new NamedPoint(1, 2, 3, "One");
+    NamedPoint thirdPoint = new NamedPoint(1, 2, 3);
+
+    @Test
+    public void testSetName() {
+        assertEquals(firstPoint.getName(), "Origin");
+        assertEquals(secondPoint.getName(), "One");
+        assertNull(thirdPoint.getName());
+    }
+
+    @Test
+    public void testGetName() {
+        firstPoint.setName("Two");
+        assertEquals(firstPoint.getName(), "Two");
+        secondPoint.setName(null);
+        assertNull(secondPoint.getName());
+        thirdPoint.setName("Five");
+        assertEquals(thirdPoint.getName(), "Five");
+    }
+}
