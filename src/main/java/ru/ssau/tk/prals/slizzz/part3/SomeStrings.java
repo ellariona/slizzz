475ccd6921c760d23d7a1872e0ce35c8b1545fdc
Елизавета <luhra@mail.ru>
2019-12-24 12:08:20 +0400

    part 3.2

--- src/main/java/ru/ssau/tk/prals/slizzz/part3/SomeStrings.java
+++ src/main/java/ru/ssau/tk/prals/slizzz/part3/SomeStrings.java
@@ -1,9 +1,19 @@
 package ru.ssau.tk.prals.slizzz.part3;
 
 public class SomeStrings {
-   static public void eachCharacterSomeStrings(String string) {
+    //task1
+    static void eachCharacterSomeStrings(String string) {
         for (int i = 0; i < string.length(); i++) {
             System.out.println(string.charAt(i));
         }
     }
+
+    //task2
+    static public void printBytesSomeStrings(String string) {
+        var bytes = string.getBytes();
+        for (byte byt : bytes) {
+            System.out.println(byt);
+        }
+    }
+
 }
--- src/test/java/ru/ssau/tk/prals/slizzz/part3/SomeStringsTest.java
+++ src/test/java/ru/ssau/tk/prals/slizzz/part3/SomeStringsTest.java
@@ -7,8 +7,18 @@ import static org.testng.Assert.*;
 public class SomeStringsTest {
     private String string = "когда каникулы :(";
 
+
+    //task1
     @Test
     public void testEachCharacterSomeStrings() {
         SomeStrings.eachCharacterSomeStrings(string);
+    }
+
+    //task2
+    @Test
+    public void testPrintBytesSomeStrings()
+    {
+        SomeStrings.printBytesSomeStrings(string);
+        System.out.println();
     }
 }
